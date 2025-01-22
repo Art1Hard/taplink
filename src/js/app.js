103c0a23e9b9cb3b2ad2954af62de9f0db6e3815
ArtHard <1artem1nazarov1@gmail.com>
2025-01-22 18:23:14 +0300

    fix major | change loading | add snowflake

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,20 @@ import isWebp from "./modules/webp.js";
 
 isWebp();
 initLoading();
+
+// Интервальная проверка для офлайн режима
+setInterval(() => {
+	if (navigator.onLine === false) {
+		document.getElementById("online-status").style.display = "block";
+	} else {
+		document.getElementById("online-status").style.display = "none";
+	}
+}, 5000);
+
+const taplinks = document.querySelectorAll(".taplink");
+
+taplinks.forEach((taplink) => {
+	taplink.addEventListener("click", () => {
+		navigator.vibrate([60, 40, 60]);
+	});
+});
--- src/js/modules/loading.js
+++ src/js/modules/loading.js
@@ -1,13 +1,11 @@
 const initLoading = () => {
 	window.addEventListener("load", () => {
 		const loader = document.querySelector(".loader");
-		setTimeout(() => {
-			document.querySelector("main").style.opacity = "1";
+		document.querySelector("main").style.opacity = "1";
 
-			loader.classList.remove("loader--show");
-			document.body.classList.remove("locked");
-			document.body.classList.add("snowflake--active");
-		}, 1500);
+		loader.classList.remove("loader--show");
+		document.body.classList.remove("locked");
+		document.body.classList.add("snowflake--active");
 	});
 };
 
